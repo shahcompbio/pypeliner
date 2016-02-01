5d8bf9871307d79a19f440336472edf643190045
Andrew McPherson <andrew.mcpherson@gmail.com>
2016-02-01 13:57:09 -0800

    install: added more metadata to setup.py and added gpl license

--- setup.py
+++ setup.py
@@ -1,6 +1,15 @@
 from setuptools import setup
 
-setup(name='pypeliner',
-      version='1.0',
-      packages=['pypeliner'],
-      )
+setup(
+    name='pypeliner',
+    packages=['pypeliner'],
+    version='0.4.1',
+    description='A library for creating informatic workflows or pipelines.',
+    author='Andrew McPherson',
+    author_email='andrew.mcpherson@gmail.com',
+    url='http://bitbucket.org/dranew/pypeliner',
+    download_url='https://bitbucket.org/dranew/pypeliner/get/v0.4.1.tar.gz',
+    keywords=['scientific', 'framework'],
+    classifiers=[],
+)
+
