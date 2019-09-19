93d390e12bae3b6163eb03725648b573f1945160
Diljot Grewal <diljot.grewal@gmail.com>
2019-09-19 12:47:40 -0400

    feature: added createtimes db for blob storage

--- pypeliner/contrib/azure/blobstorage.py
+++ pypeliner/contrib/azure/blobstorage.py
@@ -5,6 +5,7 @@ import time
 import pypeliner.flyweight
 import pypeliner.helpers
 import pypeliner.storage
+from pypeliner.sqlitedb import SqliteDb
 
 from .blobclient import BlobStorageClient, BlobMissing
 
@@ -29,7 +30,9 @@ class AzureBlob(object):
             self.filename = os.path.join(store_dir, self.filename)
             self.write_filename = os.path.join(store_dir, self.write_filename)
 
-        self.createtime_cache = storage.create_createtime_cache(self.blob_name)
+        self.createtime_cache = kwargs.get('createtime_cache')
+        self.createtime_save = kwargs.get('createtime_save')
+        self.exists_cache = kwargs.get('exists_cache')
 
     def allocate(self):
         pypeliner.helpers.makedirs(os.path.dirname(self.filename))
@@ -47,22 +50,34 @@ class AzureBlob(object):
 
         self.storage.push(self.blob_name, self.filename, create_time)
         self.createtime_cache.set(create_time)
+        self.createtime_save.set(create_time)
+        self.exists_cache.set(True)
 
     def pull(self):
         self.storage.pull(self.blob_name, self.filename)
 
     def get_exists(self):
-        createtime = self.get_createtime()
-        return createtime is not None and createtime != 'missing'
+        # raise Exception(self.exists_cache)
+        exists = self.exists_cache.get()
+        if exists is None:
+            exists = os.path.exists(self.filename)
+            self.exists_cache.set(exists)
+        return exists
 
     def get_createtime(self):
-        createtime = self.createtime_cache.get()
+        sentinel_only = pypeliner.helpers.GlobalState.get('sentinel_only')
+        if sentinel_only:
+            createtime = self.createtime_save.get()
+        else:
+            createtime = self.createtime_cache.get()
+
         if createtime is None:
             try:
                 createtime = self.storage.retrieve_blob_createtime(self.blob_name)
             except BlobMissing:
                 createtime = 'missing'
             self.createtime_cache.set(createtime)
+            self.createtime_save.set(createtime)
         if createtime == 'missing':
             return None
         createtime = datetime.datetime.strptime(createtime, '%Y/%m/%d-%H:%M:%S')
@@ -70,8 +85,10 @@ class AzureBlob(object):
 
     def touch(self):
         createtime = datetime.datetime.now().strftime('%Y/%m/%d-%H:%M:%S')
+        self.exists_cache.set(True)
         self.storage.update_blob_createtime(self.blob_name, createtime)
         self.createtime_cache.set(createtime)
+        self.createtime_save.set(createtime)
 
     def delete(self):
         self.storage.delete_blob(self.blob_name)
@@ -89,8 +106,15 @@ class AzureBlobStorage(object):
         self.tenant_id = os.environ["TENANT_ID"]
         self.subscription_id = os.environ["SUBSCRIPTION_ID"]
         self.keyvault_account = os.environ['AZURE_KEYVAULT_ACCOUNT']
-        self.cached_createtimes = pypeliner.flyweight.FlyweightState()
         self.blob_client = None
+
+        metadata_prefix = kwargs.get('metadata_prefix')
+        createtime_shelf_filename = metadata_prefix + 'createtimes.db'
+        self.cached_createtimes = pypeliner.flyweight.FlyweightState()
+        pypeliner.helpers.makedirs(os.path.dirname(createtime_shelf_filename))
+        self.saved_createtimes = pypeliner.flyweight.FlyweightState(
+            state_container=SqliteDb(createtime_shelf_filename))
+        self.cached_exists = pypeliner.flyweight.FlyweightState()
 
     def connect(self, storage_account_name):
         # dont need to regenerate client if already exists
@@ -115,25 +139,35 @@ class AzureBlobStorage(object):
         return self.cached_createtimes.create_flyweight(blob_name)
 
     def __enter__(self):
+        self.cached_exists.__enter__()
         self.cached_createtimes.__enter__()
+        self.saved_createtimes.__enter__()
         return self
 
     def __exit__(self, exc_type, exc_value, traceback):
+        self.cached_exists.__exit__(exc_type, exc_value, traceback)
         self.cached_createtimes.__exit__(exc_type, exc_value, traceback)
+        self.saved_createtimes.__exit__(exc_type, exc_value, traceback)
 
     def __getstate__(self):
-        return (self.cached_createtimes, self.rabbitmq_username, self.rabbitmq_password,
+        return (self.cached_createtimes, self.saved_createtimes, self.cached_exists,
+                self.rabbitmq_username, self.rabbitmq_password,
                 self.rabbitmq_ipaddress, self.rabbitmq_vhost, self.client_id,
                 self.secret_key, self.tenant_id, self.subscription_id,
                 self.keyvault_account)
 
     def __setstate__(self, state):
-        [self.cached_createtimes, self.rabbitmq_username, self.rabbitmq_password,
+        [self.cached_createtimes, self.saved_createtimes, self.cached_exists,
+         self.rabbitmq_username, self.rabbitmq_password,
          self.rabbitmq_ipaddress, self.rabbitmq_vhost, self.client_id,
          self.secret_key, self.tenant_id, self.subscription_id,
          self.keyvault_account] = state
 
     def create_store(self, filename, **kwargs):
+        kwargs['createtime_cache'] = self.cached_createtimes.create_flyweight(filename)
+        kwargs['createtime_save'] = self.saved_createtimes.create_flyweight(filename)
+        kwargs['exists_cache'] = self.cached_exists.create_flyweight(filename)
+
         return AzureBlob(self, filename, **kwargs)
 
     def get_storage_account(self, filename):
