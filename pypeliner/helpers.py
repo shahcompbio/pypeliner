0c87ae4d3f42ffa5d76b76734696bb29c93a8df0
Diljot Grewal <diljot.grewal@gmail.com>
2024-02-15 11:51:53 -0800

    slurm Queue

--- pypeliner/helpers.py
+++ pypeliner/helpers.py
@@ -211,6 +211,7 @@ class MultiLineFormatter(logging.Formatter):
 
 class JsonFormatter(logging.Formatter):
     def format(self, record):
+        print(record)
         return json.dumps(vars(record))
 
 
